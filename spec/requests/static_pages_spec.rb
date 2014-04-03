67169a73da2ef0acb8c7e3bb2521c0fb5c2fc8b2
Marc Destefanis <marc.destefanis.etu@gmail.com>
2014-04-03 07:29:12 -0700

    TP3.4

---
+++ spec/requests/static_pages_spec.rb
@@ -0,0 +1,21 @@
+require 'spec_helper'
+describe "Static pages" do
+  describe "Help page" do
+  it "should have the content 'Help'" do
+    visit '/static_pages/help'
+    page.should have_content('Help')
+  end
+end
+  describe "Home page" do
+    it "should have the right title" do
+      visit '/static_pages/home'
+      page.should have_selector('title',:text => "Ruby on Rails Tutorial Example App | Home")
+    end
+  end
+  describe "About page" do
+    it "should have the content 'About'" do
+      visit '/static_pages/about'
+      page.should have_content('About')
+    end
+  end
+end
