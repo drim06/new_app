a90e9f699a46336fd062c52ac1548272427387b2
Marc Destefanis <marc.destefanis.etu@gmail.com>
2014-05-22 06:10:53 -0700

    debut TP11

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -16,8 +16,8 @@ describe "User pages" do
 
     before { visit signup_path }
 
-    it{ should have_selector('h1', text: 'Sign up')}
     it{ should have_selector('title', text: 'Sign up')}
+    it{ should have_selector('h1', text: 'Sign up')}
 
     
   end
